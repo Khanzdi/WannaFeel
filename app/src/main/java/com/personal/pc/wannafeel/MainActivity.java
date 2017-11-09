bd374b0ad58bdaadabc8a6a7796c452e992f1158
KEVIN-ORTIZ\Personal <khanzdi@unac.edu.co>
2017-11-08 20:53:50 -0500

    Added Support for API 24 or Higher

--- app/src/main/java/com/personal/pc/wannafeel/MainActivity.java
+++ app/src/main/java/com/personal/pc/wannafeel/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -35,6 +36,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends Activity {
@@ -45,6 +48,7 @@ public class MainActivity extends Activity {
     private EditText mEditText;
     private Bitmap bMap;
     private EmotionServiceClient client;
+    String mCurrentPhotoPath;
 
 
     @Override
@@ -65,16 +69,7 @@ public class MainActivity extends Activity {
         btn_hacerfoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cameraIntent = new Intent(
-                        android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                File imagesFolder = new File(
-                        Environment.getExternalStorageDirectory(), "Wannafeel");
-                imagesFolder.mkdirs();
-                File image = new File(imagesFolder, "foto.jpg");
-                Uri uriSavedImage = Uri.fromFile(image);
-
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
-                startActivityForResult(cameraIntent, 1);
+                dispatchTakePictureIntent();
             }
         });
         salir.setOnClickListener(new View.OnClickListener() {
@@ -85,11 +80,53 @@ public class MainActivity extends Activity {
         });
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                System.out.println(ex.toString());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1);
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1 && resultCode == RESULT_OK) {
-             bMap = BitmapFactory.decodeFile(
-                    Environment.getExternalStorageDirectory() +
-                            "/Wannafeel/" + "foto.jpg");
+
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            File f = new File(mCurrentPhotoPath);
+            Uri contentUri = Uri.fromFile(f);
+            mediaScanIntent.setData(contentUri);
+            this.sendBroadcast(mediaScanIntent);
+
+            bMap = BitmapFactory.decodeFile(mCurrentPhotoPath);
             img.setImageBitmap(bMap);
             btnProcesar.setVisibility(View.VISIBLE);
         }
